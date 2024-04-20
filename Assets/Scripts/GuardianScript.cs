83520d3cdd5748024347d45e1a0d852b362780ef
Diego_Montoya <0258630@up.edu.mx>
2024-04-20 15:33:48 -0600

    Se añade IA a los guardianes

--- Assets/Scripts/GuardianScript.cs
+++ Assets/Scripts/GuardianScript.cs
@@ -1,39 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.AI;
-
 public class GuardianScript : MonoBehaviour
 {
-    public NavMeshAgent agent;
-    public GameObject destination;
-    public int counter = 0;
-    private Vector3[] positionArray = new[] {new Vector3(-0.15f, 0.21f, 0.99f), new Vector3(14.45f, 0.21f, 6.83f), new Vector3(6.08f, 0.21f, -12.79f) };
-    
+    public Vector3[] puntosDeRecorrido = new Vector3[]
+    {
+        new Vector3(9.5f, 0.21f, 11.84f),
+        new Vector3(14.68f, 0.21f, 6.86f),
+        new Vector3(0.06f, 0.21f, -4.4f),
+        new Vector3(6.55f, 0.21f, -13.05f),
+        new Vector3(11.34f, 0.21f, -4.34f),
+        new Vector3(13.11f, 0.21f, -13.05f)
+    };
+
+    private NavMeshAgent agenteNavMesh;
+    private int indiceDestinoActual = 0;
 
     void Start()
     {
-        agent.destination = destination.transform.position;
-        
-    }
+        agenteNavMesh = GetComponent<NavMeshAgent>();
+        // Elige una posición aleatoria del arreglo como posición inicial
+        int indicePosicionInicial = UnityEngine.Random.Range(0, puntosDeRecorrido.Length);
+        transform.position = puntosDeRecorrido[indicePosicionInicial];
 
-
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(counter >= 3)
+        // Inicia el recorrido con el primer punto de la lista
+        if (puntosDeRecorrido.Length > 0)
         {
-            counter = 0;
+            agenteNavMesh.SetDestination(puntosDeRecorrido[indicePosicionInicial]);
+            indiceDestinoActual = indicePosicionInicial;
         }
-        destination.transform.position = positionArray[counter];
-        counter++;
-
     }
 
-    // Update is called once per frame
     void Update()
     {
-        agent.destination = destination.transform.position;
+        // Verifica si el agente llegó a su destino actual
+        if (!agenteNavMesh.pathPending && agenteNavMesh.remainingDistance < 0.1f)
+        {
+            // Avanza al siguiente destino en el recorrido
+            SiguienteDestino();
+        }
+    }
+
+    void SiguienteDestino()
+    {
+        // Selecciona aleatoriamente el próximo destino
+        int nuevoIndice = UnityEngine.Random.Range(0, puntosDeRecorrido.Length);
+        // Asegura que el nuevo índice no sea el mismo que el actual
+        while (nuevoIndice == indiceDestinoActual)
+        {
+            nuevoIndice = UnityEngine.Random.Range(0, puntosDeRecorrido.Length);
+        }
+        // Establece el nuevo destino
+        indiceDestinoActual = nuevoIndice;
+        agenteNavMesh.SetDestination(puntosDeRecorrido[indiceDestinoActual]);
     }
 }
