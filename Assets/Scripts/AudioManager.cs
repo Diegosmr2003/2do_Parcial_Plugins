a8c539192c364a162c0fda365f0627742d97a321
0246960 <0246960@up.edu.mx>
2024-04-25 15:47:32 -0600

    Ya sirve la corutina con audio de background y near guardian

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -8,18 +8,18 @@ public class AudioManager : MonoBehaviour
 
 
     [Header("------------Audio Clip-------------")]
-    public AudioClip background;
+    //public AudioClip background;
     public AudioClip gunshoot;
     public AudioClip gunnoammo;
-    public AudioClip chase;
-    public AudioClip nearguardian;
+    //public AudioClip chase;
+    //public AudioClip nearguardian;
 
-    private void Start()
+    /*private void Start()
     {
-        musicSource.clip = background;
-        musicSource.Play();
+        //musicSource.clip = background;
+        //musicSource.Play();
       
-    }
+    }*/
 
     public void playSFX(AudioClip clip) //Este método es público para que se pueda acceder a él mediante otros scripts
     {
---
+++ Assets/Scripts/MonsterSFX.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSFX : MonoBehaviour
+{
+    public Transform player; // Referencia al jugador 
+    public AudioClip backgroundMusic; // Música de fondo
+    public AudioClip approachMusic;
+    public AudioClip chaseMusic;
+
+    private AudioSource audioSource;
+    private bool isChasing = false;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = backgroundMusic;
+        audioSource.Play();
+        StartCoroutine(CheckPlayerDistance());
+    }
+
+    IEnumerator CheckPlayerDistance()
+    {
+        while (true)
+        {
+            while (true)
+            {
+                float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+                if (distanceToPlayer < 10f && distanceToPlayer > 1 /*&& !isChasing*/)
+                {
+                    ChangeMusic(approachMusic);
+                }
+                else if (distanceToPlayer >= 20f)
+                {
+                    ChangeMusic(backgroundMusic);
+                }
+
+                yield return new WaitForSeconds(1f); // Comprueba la distancia cada segundo
+            }
+        }
+    }
+
+    void ChangeMusic(AudioClip newClip)
+    {
+        if (audioSource.clip != newClip)
+        {
+            audioSource.Stop();
+            audioSource.clip = newClip;
+            audioSource.Play();
+        }
+    }
+}
