829ee4438f37f0cc8a2f1fd2a8d96eae95004e64
Giorgio <0245125@up.edu.mx>
2024-05-21 21:40:33 -0600

    Camara tercera persona mejorada y boss room

--- Assets/Scripts/CameraChange.cs
+++ Assets/Scripts/CameraChange.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class CameraChange : MonoBehaviour //Este codigo es exclusivamente para el cambio del efecto de camara
 {
-    public GameObject mainCamera; //Declaramos un gameobject que será la cámara del jugador
+    public GameObject mainCamera1; //Declaramos un gameobject que será la cámara del jugador
+    public GameObject mainCamera2;
 
     void Start()
     {
-        mainCamera.GetComponent<Effects>(); //Obtenemos el componente de "effects", que es el que venia con Effects Pro que descargamos en clase una vez
+        mainCamera1.GetComponent<Effects>(); //Obtenemos el componente de "effects", que es el que venia con Effects Pro que descargamos en clase una vez
+        mainCamera2.GetComponent<Effects>();
     }
 
     public void activateEffect() //Hacemos una función que active el efecto
     {
-        if (mainCamera.GetComponent<Effects>().enabled != true){ //Si no está activado previamente
-            mainCamera.GetComponent<Effects>().enabled = true; //Lo activa
+        if (mainCamera1.GetComponent<Effects>().enabled != true){ //Si no está activado previamente
+            mainCamera1.GetComponent<Effects>().enabled = true; //Lo activa
         }
-        
+
+        if (mainCamera2.GetComponent<Effects>().enabled != true)
+        { 
+            mainCamera2.GetComponent<Effects>().enabled = true; //Lo activa
+        }
     }
 
     public void deactivateEffect() //Hacemos una función que desactive el efecto
     {
-        if (mainCamera.GetComponent<Effects>().enabled != false){ //Si no está desactivado previamente
-            mainCamera.GetComponent<Effects>().enabled = false; //Lo desactiva
+        if (mainCamera1.GetComponent<Effects>().enabled != false){ //Si no está desactivado previamente
+            mainCamera1.GetComponent<Effects>().enabled = false; //Lo desactiva
+        }
+
+        if (mainCamera2.GetComponent<Effects>().enabled != false)
+        { 
+            mainCamera2.GetComponent<Effects>().enabled = false; //Lo desactiva
         }
     }
 }
