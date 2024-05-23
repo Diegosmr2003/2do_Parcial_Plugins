6aad2e2c2f96aba10e3d5e669ab3afc7bc261705
unknown <CIAC@CIAC1-004.academic.mixcoac.upmx.mx>
2024-05-22 18:58:45 -0600

    Se sigue trabajando en la camara

--- Assets/Scripts/CamSwitch.cs
+++ Assets/Scripts/CamSwitch.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CamSwitch : MonoBehaviour
 {
     public GameObject cam1;
     public GameObject cam2;
+    public GameObject gun;
 
     // Update is called once per frame
     void Update()
@@ -14,11 +16,13 @@ public class CamSwitch : MonoBehaviour
         {
             cam1.SetActive(true);
             cam2.SetActive(false);
+            gun.transform.parent = cam1.transform;
         }
         if (Input.GetButtonDown("2Key"))
         {
             cam1.SetActive(false);
             cam2.SetActive(true);
+            gun.transform.parent = cam2.transform;
         }
 
     }
